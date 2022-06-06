e3f75b9c37d9a711611dd6c9e45c36429fa74be7
lojy4231 <lojy4231@gmail.com>
2022-06-06 22:26:35 +0900

    두가지 빼고 테스트 완료

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ class Board {
         const randomId = Math.random().toString(36).substring(2, 16);
         article.id = `${this.name}-${randomId}`;
         const date = new Date();
-        article.createdDate = date.toISOString(); 
-        
+        article.createdDate = date.toISOString();
+
         this.articles.push(article);
 
         // throw new Error('사이트에 없는 보드임');
@@ -42,22 +42,48 @@ class Board {
 }
 
 class Article {
-    constructor({ subject, content, author, id, createdDate }) {
-        console.log('이거슨 서브젝트:', subject);
+    constructor({ subject, content, author }) {
+        if (subject === '' || subject === null) {
+            throw new Error('빈배열은 용납 못함');
+        } else if(content === '' || content === null){
+            throw new Error('빈배열은 용납 못함');
+        }else if(author === '' || author === null){
+            throw new Error('빈배열은 용납 못함');
+        }
         this.subject = subject;
         this.content = content;
         this.author = author;
-        this.id = id;
-        this.createdDate = createdDate;
+
         this.comment = [];
     }
+
+    reply(comment){
+        const date = new Date();
+        comment.createdDate = date.toISOString();
+
+        this.comment.push(comment);
+        console.log('이거슨 코멘트 등록:', comment);
+    }
+
+    getAllComments(){
+        const commentList = [];
+        this.comment.forEach((comment) => commentList.push(comment));
+        return commentList;
+    }
+
+
+
 }
 
 class Comment {
-    constructor(content, author, createdDate) {
+    constructor({content, author}) {
+        if (content === '' || content === null) {
+            throw new Error('빈배열은 용납 못함');
+        } else if(author === '' || author === null){
+            throw new Error('빈배열은 용납 못함');
+        }
         this.content = content;
         this.author = author;
-        this.date = createdDate;
     }
 }
 
