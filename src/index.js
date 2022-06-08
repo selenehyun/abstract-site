47fcaee821211631aab14a2052bc951aeac89927
alpha-fly <placetobe@naver.com>
2022-06-08 11:00:06 +0900

    test 모두 통과

--- src/index.js
+++ src/index.js
@@ -1,10 +1,111 @@
-class Site {}
+class Site {
+    constructor() {        
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        try {
+            if ( this.boards.find(item => item['name'] === board.name ) ) throw new Error('이름이 같은 board가 있습니다.');                         
+            board.added = true
+            this.boards.push(board); 
+            
+        } catch (error) {
+            throw error;
+        }
+    }
 
-class Article {}
+    findBoardByName(name) {
+        return this.boards.find(item => item['name'] === name )     
+    }    
+}
 
-class Comment {}
+class Board {
+    constructor(name) {
+        this.name = name;        
+        this.articles = [];       
+        this.added = false;
+
+        try {
+            if (name === '' || name === null) throw new Error('빈 문자열이나 null은 안됩니다.');
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    publish(article) {                  
+        try {                                   
+     
+            if (this.added) {
+                article.id = this.name + "-" + Math.random();
+                article.published = true;                
+                this.articles.push(article);
+                            
+            } else {
+                throw new Error('Site에 추가된 Board에서만 Article을 추가할 수 있습니다.');
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    getAllArticles() {
+        return this.articles;
+    }
+
+}
+
+class Article {
+    constructor(Article) {
+
+        try {
+            if (Article["subject"] === '' || Article["subject"] === null) throw new Error('빈 문자열이나 null은 안됩니다.');
+            if (Article["content"] === '' || Article["content"] === null) throw new Error('빈 문자열이나 null은 안됩니다.');
+            if (Article["author"] === '' || Article["author"] === null) throw new Error('빈 문자열이나 null은 안됩니다.');
+        } catch (error) {
+            throw error;
+        }        
+        this.subject = Article["subject"];
+        this.content = Article["content"];
+        this.author = Article["author"];        
+        this.createdDate = new Date().toISOString();       
+        this.published = false;
+        this.comments = [];
+    }
+
+    reply(comment) {
+        try {                                   
+     
+            if (this.published) {                                               
+                this.comments.push(comment);
+                            
+            } else {
+                throw new Error('Board에 추가된 Article에서만 Comment를 추가할 수 있습니다.');
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    getAllComments() {
+        return this.comments;
+    }
+
+}
+
+class Comment {
+    constructor(comment) {        
+        try {           
+            if (comment["content"] === '' || comment["content"] === null) throw new Error('빈 문자열이나 null은 안됩니다.');
+            if (comment["author"] === '' || comment["author"] === null) throw new Error('빈 문자열이나 null은 안됩니다.');
+        } catch (error) {
+            throw error;
+        }    
+
+        this.content = comment["content"];
+        this.author = comment["author"];
+        this.createdDate = new Date().toISOString();       
+    }
+}
 
 module.exports = {
     Site,
